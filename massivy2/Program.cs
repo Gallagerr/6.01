737ff04590fa595ff44044c44c696feb774edee3
sadiev <sadiev@itstep.kz>
2019-04-16 18:52:16 +0600

    Добавьте файлы проекта.

---
+++ massivy2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace massivy2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Введите 6 значный билет");
+      try
+      {
+        string sticket = Console.ReadLine();
+        if (sticket.Length < 0 && sticket.Length > 6)
+        {
+          throw new IndexOutOfRangeException();
+        }
+        int Iticket = Convert.ToInt32(sticket);
+        Console.WriteLine((Convert.ToInt32(Convert.ToString(sticket)[0].ToString()) +
+                           Convert.ToInt32(Convert.ToString(sticket)[1].ToString()) +
+                           Convert.ToInt32(Convert.ToString(sticket)[2].ToString())) ==
+                          (Convert.ToInt32(Convert.ToString(sticket)[3].ToString()) +
+                           Convert.ToInt32(Convert.ToString(sticket)[4].ToString()) +
+                           Convert.ToInt32(Convert.ToString(sticket)[5].ToString()))
+            ? "Билет счастливый"
+            : "Билет не является счастливым");
+      }
+      catch (Exception)
+      {
+        Console.WriteLine("Завершение программы");
+        Environment.Exit(0);
+      }
+  }
+}
---
+++ massivy3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace massivy3
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      int spaceValue = 0;
+      ConsoleKeyInfo symbol;
+      do
+      {
+        Console.WriteLine("Введите символ для конвертации регистра");
+        Console.WriteLine("Для окончания ввода нажмите ESC ");
+        symbol = Console.ReadKey();
+        Console.WriteLine();
+        Console.Clear();
+        Console.WriteLine("Введенный символ " + symbol.KeyChar);
+        if (char.IsLetter(Convert.ToChar(symbol.KeyChar)))
+        {
+          if (char.IsUpper(Convert.ToChar(symbol.KeyChar)))
+          {
+            Console.WriteLine("Конвертируемый символ " + char.ToLower(Convert.ToChar(symbol.KeyChar)));
+          }
+          else
+          {
+            Console.WriteLine("Конвертируемый символ " + char.ToUpper(Convert.ToChar(symbol.KeyChar)));
+          }
+        }
+
+        if (Convert.ToInt32(symbol.KeyChar) == 27)
+        {
+          break;
+        }
+      } while (true);
+    }
+  }
+}
